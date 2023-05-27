2f9b067e6f505fa2fa3638275d6aeb4454466238
guzio <star.master.123.mg@gmail.com>
2023-05-27 11:24:46 +0200

    We goin' jail...

--- app/src/main/java/com/pribus/driver/ui/FuckingRAPING.java
+++ app/src/main/java/com/pribus/driver/ui/FuckingRAPING.java
@@ -1,24 +1,21 @@
-package com.pribus.driver.ui.routes;
+package com.pribus.driver.ui;
 
-import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 
-import com.pribus.driver.MainActivity;
 import com.pribus.driver.R;
-import com.pribus.driver.ui.BillingFragment;
 
 /**
- * A fragment representing a list of Items.
+ * A simple {@link Fragment} subclass.
+ * Use the {@link FuckingRAPING#newInstance} factory method to
+ * create an instance of this fragment.
  */
-public class RoutesListFragment extends Fragment {
+public class FuckingRAPING extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,7 +26,7 @@ public class RoutesListFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public RoutesListFragment() {
+    public FuckingRAPING() {
         // Required empty public constructor
     }
 
@@ -39,11 +36,11 @@ public class RoutesListFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment BillingFragment.
+     * @return A new instance of fragment FuckingRAPING.
      */
     // TODO: Rename and change types and number of parameters
-    public static BillingFragment newInstance(String param1, String param2) {
-        BillingFragment fragment = new BillingFragment();
+    public static FuckingRAPING newInstance(String param1, String param2) {
+        FuckingRAPING fragment = new FuckingRAPING();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -54,13 +51,6 @@ public class RoutesListFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        MainActivity.i.bottom = (ImageView) MainActivity.i.findViewById(R.id.fuckthismap);
-        /*MainActivity.i.bottom.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MainActivity.i.bottom.setVisibility(View.INVISIBLE);
-            }
-        });*/
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -71,6 +61,6 @@ public class RoutesListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_routes_list, container, false);
+        return inflater.inflate(R.layout.fragment_rate_list, container, false);
     }
 }
